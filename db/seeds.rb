ce29104b48d9aea0e606227e14ffbfa50b5df9d4
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-21 16:22:18 -0400

    Stub out basic product seed * Update fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,18 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+products = Rails.root.join("test", "fixtures", "products.yml")
+
+YAML.load_file(products).each do |name, attributes|
+  puts "Creating #{attributes["title"]} ..."
+
+  product = Product.find_or_create_by(title: attributes["title"])
+  product.description = attributes["description"],
+  product.price = attributes["price"]
+  product.image.attach(
+    io: File.open(
+      Rails.root.join("test", "fixtures", "files", attributes["image"])
+    ),
+    filename: attributes["title"]
+  )
+  product.save!
+
+  puts "  > #{attributes["title"]} created! ✅\n\n"
+end
