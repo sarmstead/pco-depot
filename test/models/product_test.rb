d0291ef303ef392830e88f6c348be6cdb38f5a27
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-22 15:58:43 -0400

    Add product model test for non-empty attributes

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:description].any?
+    assert product.errors[:image].any?
+    assert product.errors[:price].any?
+    assert product.errors[:title].any?
+  end
 end
