663fe6d5020e997fa0be78549683faa345b1f401
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-23 12:46:09 -0400

    Use fixture instead of memoized product instance

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  setup do
+    @product = products(:drill)
+  end
+
   test "product attributes must not be empty" do
     invalid_product = Product.new
     assert invalid_product.invalid?
@@ -11,53 +15,53 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "should be invalid when price is negative" do
-    product.price = -1
-    assert product.invalid?
-    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+    @product.price = -1
+    assert @product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], @product.errors[:price]
   end
 
   test "should be invalid when price is zero" do
-    product.price = 0
-    assert product.invalid?
-    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+    @product.price = 0
+    assert @product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], @product.errors[:price]
   end
 
   test "should be valid when price is greater than or equal to one cent" do
-    product.price = 0.01
-    assert product.valid?
+    @product.price = 0.01
+    assert @product.valid?
 
-    product.price = 0.02
-    assert product.valid?
+    @product.price = 0.02
+    assert @product.valid?
   end
 
   test ".png image file extension is valid" do
-    assert product.valid?, "image/png must be valid"
+    assert @product.valid?, "image/png must be valid"
   end
 
   test ".gif image file extension is valid" do
-    product.image.attach(
+    @product.image.attach(
       io: File.open(
         file_fixture("test.gif"),
       ),
       filename: "test.gif",
       content_type: "image/gif"
     )
-    assert product.valid?, "image/gif must be valid"
+    assert @product.valid?, "image/gif must be valid"
   end
 
   test ".jpg image file extension is valid" do
-    product.image.attach(
+    @product.image.attach(
       io: File.open(
         file_fixture("test.jpg"),
       ),
       filename: "test.jpg",
       content_type: "image/jpeg"
     )
-    assert product.valid?, "image/jpeg must be valid"
+    assert @product.valid?, "image/jpeg must be valid"
   end
 
   test "any image file extension other than .gif, .png, or .jpg is valid" do
-    product.image.attach(
+    @product.image.attach(
       io: File.open(
         file_fixture("test.svg"),
       ),
@@ -65,26 +69,6 @@ class ProductTest < ActiveSupport::TestCase
       content_type: "image/svg"
     )
 
-    assert_not product.valid?, "image/svg must not be valid"
-  end
-
-  private
-
-  def product
-    @product ||= begin
-      product = Product.new(
-        title: "Power Gloves",
-        description: "Pro rated work gloves with fire protection.",
-        price: 12.34
-      )
-      product.image.attach(
-        io: File.open(
-          file_fixture("power-gloves.png")
-        ),
-        filename: "power-gloves.png",
-        content_type: "image/png"
-      )
-      product
-    end
+    assert_not @product.valid?, "image/svg must not be valid"
   end
 end
