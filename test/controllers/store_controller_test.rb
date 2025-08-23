7654740367e35d9b0572bd6d737cc132eb8b1cc0
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-23 17:34:33 -0400

    Update test expectations for storefront index

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class StoreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  setup do
+    @product = products(:drill)
+  end
+
+  test "should get index and display some products" do
     get store_index_url
     assert_response :success
+    assert_select "nav li", minimum: 3
+    assert_select "h1", "Products"
+    assert_select "h2", @product.title
+    assert_select "p", /\$[,\d]+\.\d\d/
   end
 end
