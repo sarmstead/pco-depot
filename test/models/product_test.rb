4165e05aebd7b96a2bbb258cba8de3b5a9fc69ef
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-23 11:39:08 -0400

    Add tests for price validations

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -2,11 +2,46 @@ require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
   test "product attributes must not be empty" do
-    product = Product.new
+    invalid_product = Product.new
+    assert invalid_product.invalid?
+    assert invalid_product.errors[:description].any?
+    assert invalid_product.errors[:image].any?
+    assert invalid_product.errors[:price].any?
+    assert invalid_product.errors[:title].any?
+  end
+
+  test "should be invalid when price is negative" do
+    product.price = -1
     assert product.invalid?
-    assert product.errors[:description].any?
-    assert product.errors[:image].any?
-    assert product.errors[:price].any?
-    assert product.errors[:title].any?
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+  end
+
+  test "should be invalid when price is zero" do
+    product.price = 0
+    assert product.invalid?
+    assert_equal [ "must be greater than or equal to 0.01" ], product.errors[:price]
+  end
+
+  test "should be valid when price is greater than or equal to one cent" do
+    product.price = 0.01
+    assert product.valid?
+
+    product.price = 0.02
+    assert product.valid?
+  end
+
+  private
+
+  def product
+    @product ||= begin
+      product = Product.new(title: "Power Gloves", description: "Pro rated work gloves with fire protection.")
+      product.image.attach(
+        io: File.open(
+          file_fixture("power-gloves.png")
+        ),
+        filename: "power-gloves.png"
+      )
+      product
+    end
   end
 end
