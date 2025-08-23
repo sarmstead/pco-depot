8b46ec0bb3b2094417fd4b45bd456ac051ec4731
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-23 12:42:07 -0400

    Rename product fixtures and update references

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class ProductsTest < ApplicationSystemTestCase
   setup do
-    @product = products(:one)
+    @product = products(:drill)
   end
 
   test "visiting the index" do
