136aca5fd21aa8a58b3190f5b2f95eb1eb0759a7
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-30 12:24:24 -0400

    Add total_price to cart instances * Add test to assert line_item.total_price instances are called

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
+require "minitest/mock"
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#total_price calls line_item.total_price" do
+    line_item = Minitest::Mock.new
+    line_item.expect(:total_price, 1)
+
+    cart = Cart.new
+    cart.stub :line_items, [ line_item ] do
+      cart.total_price
+    end
+
+    assert_mock line_item
+  end
 end
