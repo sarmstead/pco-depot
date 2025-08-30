73e47e5d340b2a09ed5d9732e6342ea3aa4a82f2
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-08-30 12:21:07 -0400

    Add total_price to line_item instances * Add test for new method

--- test/models/line_item_test.rb
+++ test/models/line_item_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class LineItemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#total_price returns product price multiplied by quantity" do
+    manual_total = line_items(:one).product.price * line_items(:one).quantity
+    assert line_items(:one).total_price == manual_total
+  end
 end
