f0232e84a2a19fca8bfc384d089b7274087b0738
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-09-07 23:29:47 -0400

    Update cart system tests

--- test/system/carts_test.rb
+++ test/system/carts_test.rb
@@ -11,29 +11,18 @@ class CartsTest < ApplicationSystemTestCase
   end
 
   test "should create cart" do
-    visit carts_url
-    click_on "New cart"
+    visit store_index_url
+    click_on "Add to Cart", match: :first
+    click_on "Cart"
 
-    click_on "Create Cart"
-
-    assert_text "Cart was successfully created"
-    click_on "Back"
-  end
-
-  test "should update Cart" do
-    visit cart_url(@cart)
-    click_on "Edit this cart", match: :first
-
-    click_on "Update Cart"
-
-    assert_text "Cart was successfully updated"
-    click_on "Back"
+    assert_text "Cart"
+    assert_text "Order Summary"
   end
 
   test "should destroy Cart" do
     visit cart_url(@cart)
-    accept_confirm { click_on "Destroy this cart", match: :first }
+    click_on "Empty Cart"
 
-    assert_text "Cart was successfully destroyed"
+    assert_current_path store_index_url
   end
 end
