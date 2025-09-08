eef83b284503f977162cbd9632eacba0a2305931
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-09-08 02:37:46 -0400

    Add charge! method

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,4 +11,37 @@ class Order < ApplicationRecord
       line_items << item
     end
   end
+
+  def charge!(params)
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type
+    when "check"
+      payment_method = :check
+      payment_details[:routing] = params[:routing_number]
+      payment_details[:account] = params[:account_number]
+    when "credit_card"
+      payment_method = :credit_card
+      month, year = params[:expiration_date].split(//)
+      payment_details[:cc_num] = params[:credit_card_number]
+      payment_details[:expiration_month] = month
+      payment_details[:expiration_year] = year
+    when "purchase_order"
+      payment_method = :po
+      payment_details[:po_num] = params[:po_number]
+    end
+
+    payment_result = Pago.make_payment(
+      order_id: id,
+      payment_method: payment_method,
+      payment_details: payment_details
+    )
+
+    if payment_result.succeeded?
+      OrderMailer.received(self).deliver_later
+    else
+      raise payment_result.error
+    end
+  end
 end
