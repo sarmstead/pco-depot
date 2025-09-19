43721d062f739c7d167e352a6da25edc5d46885f
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-09-19 10:27:55 -0400

    Add user validations, and update tests and fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email_address, presence: true, uniqueness: true
+
   has_secure_password
+
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
