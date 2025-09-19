064ae97847f67b5131fa71cf62d1f2ed31a1d045
Sunjay Armstead <65554107+sarmstead@users.noreply.github.com>
2025-09-19 11:04:27 -0400

    Remove email from confirmation flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to store_index_path, notice: "#{@user.first_name}(#{@user.email_address})'s profile was successfully created!🎉" }
+        format.html { redirect_to store_index_path, notice: "#{@user.first_name}'s profile was successfully created!🎉" }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to store_index_path, notice: "#{@user.first_name}(#{@user.email_address})'s profile was successfully updated.", status: :see_other }
+        format.html { redirect_to store_index_path, notice: "#{@user.first_name}'s profile was successfully updated.", status: :see_other }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
